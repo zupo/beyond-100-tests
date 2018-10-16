641ec8ebff2cf09e357cb875af91f45423146e47
Nejc Zupan <nejc.zupan@gmail.com>
2018-10-16 07:46:26 +0200

    Enums: You know what's awesome? Less code!

--- run.py
+++ run.py
@@ -1,41 +1,30 @@
-def colorize(text, color):
-    if color == "red":
-        color = "1;31"
-    elif color == "blue":
-        color = "1;34"
-    else:
-        raise Exception(f"Unknown color: {color}")
+import enum
 
-    return f"\033[{color}m{text}\033[0m"
+
+class Color(enum.Enum):
+    red = "1;31"
+    blue = "1;34"
+
+
+def colorize(text, color: Color):
+    return f"\033[{color.value}m{text}\033[0m"
 
 
 if __name__ == "__main__":  # pragma: no cover
-    print(f"roses are {colorize('red', 'red')}")
-    print(f"this is a {colorize('hack', 'black')}")
+    print(f"roses are {colorize('red', Color.red)}")
+    print(f"this is a {colorize('hack', Color.black)}")
 
 
 # -- HERE BE TESTS -- #
 
 
 def test_red():
-    assert colorize("foo", "red") == "\033[1;31mfoo\033[0m"
-
-
-def test_blue():
-    assert colorize("foo", "blue") == "\033[1;34mfoo\033[0m"
-
-
-def test_black():
-    import pytest
-
-    with pytest.raises(Exception) as exc:
-        colorize("foo", "black")
-
-    assert str(exc.value) == "Unknown color: black"
+    assert colorize("foo", Color.red) == "\033[1;31mfoo\033[0m"
 
 
 """ Scenario:
 $ pytest --cov=run --cov-report html run.py --cov-branch
 $ open htmlcov/index.html
+$ mypy run.py
 $ python run.py
 """
