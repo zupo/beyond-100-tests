cc72f1cbba1d15d093e4978e538485d8c1fdddf0
Nejc Zupan <nejc.zupan@gmail.com>
2018-10-16 07:42:52 +0200

    Branch Coverage, step 3: Back to 100% coverage (with branches)

--- run.py
+++ run.py
@@ -3,6 +3,8 @@ def colorize(text, color):
         color = "1;31"
     elif color == "blue":
         color = "1;34"
+    else:
+        raise Exception(f"Unknown color: {color}")
 
     return f"\033[{color}m{text}\033[0m"
 
@@ -23,7 +25,17 @@ def test_blue():
     assert colorize("foo", "blue") == "\033[1;34mfoo\033[0m"
 
 
+def test_black():
+    import pytest
+
+    with pytest.raises(Exception) as exc:
+        colorize("foo", "black")
+
+    assert str(exc.value) == "Unknown color: black"
+
+
 """ Scenario:
 $ pytest --cov=run --cov-report html run.py --cov-branch
 $ open htmlcov/index.html
+$ python run.py
 """
