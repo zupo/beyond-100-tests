c7758831b979c50f16d2b987b42d645b34a5b30d
Nejc Zupan <nejc.zupan@gmail.com>
2018-11-06 15:29:09 +0900

    flake8_builtins: Beware of builtins!
    Use flake8_builtins to notify you when you are overriding builtins which
    can cause all sorts of nasty bugs.

--- run.py
+++ run.py
@@ -1,30 +1,30 @@
-import enum
+def max_values(list, list2):
+    max = list[0]
+    for x in list:
+        if x > 0:
+            max = x
 
+    all_values = list()
+    all_values.append(max)
 
-class Color(enum.Enum):
-    red = "1;31"
-    blue = "1;34"
+    max = list2[0]
+    for x in list2:
+        if x > 0:
+            max = x
+    all_values.append(max)
 
-
-def colorize(text, color: Color):
-    return f"\033[{color.value}m{text}\033[0m"
-
-
-if __name__ == "__main__":  # pragma: no cover
-    print(f"roses are {colorize('red', Color.red)}")
-    print(f"this is a {colorize('hack', Color.black)}")
+    return all_values
 
 
 # -- HERE BE TESTS -- #
 
 
-def test_red():
-    assert colorize("foo", Color.red) == "\033[1;31mfoo\033[0m"
+def test_max_values():
+    assert max_values([3, 4, 5], [5, 6, 7]) == [5, 7]
 
 
 """ Scenario:
-$ pytest --cov=run --cov-report html run.py --cov-branch
-$ open htmlcov/index.html
-$ mypy run.py
-$ python run.py
+$ pytest run.py
+$ flake8 run.py
+$ open https://pypi.org/project/flake8-builtins/
 """
