2df1733e08e78966ed98bae1be804edde56eb35a
Nejc Zupan <nejc.zupan@gmail.com>
2018-11-06 16:08:55 +0900

    Mocking time, step 2: Only slightly change the import and test breaks :(
    Because we had to mock the calls of internal code of
    get_current_microsecond(), if this internal code changes ever so slightly,
    our test will fail and that's quite annoying.

--- run.py
+++ run.py
@@ -1,8 +1,12 @@
-from datetime import datetime
+import datetime
+
+
+def get_yesterdays_microsecond():
+    return datetime.datetime.now().microsecond - datetime.timedelta(day=1)
 
 
 def get_current_microsecond():
-    return datetime.now().microsecond
+    return datetime.datetime.now().microsecond
 
 
 # -- HERE BE TESTS -- #
