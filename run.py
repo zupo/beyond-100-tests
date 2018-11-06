ea05a6b3fb94d74f43fdd00c21f0141667979892
Nejc Zupan <nejc.zupan@gmail.com>
2018-11-06 19:14:05 +0900

    Namedtuples, step 2: You know what's awesome? Less code!
    Let the computer take care of checking that types match, so we don't
    have to.

--- run.py
+++ run.py
@@ -1,9 +1,10 @@
-def describe_car(car):
-    if not "wheels" in car.keys():
-        raise Exception("wheels is a required key")
-    if not "doors" in car.keys():
-        raise Exception("doors is a required key")
-    return f"This car has {car['wheels']} wheels and {car['doors']} doors."
+from collections import namedtuple
+
+Car = namedtuple("Car", ["wheels", "doors"])
+
+
+def describe_car(car: Car):
+    return f"This car has {car.wheels} wheels and {car.doors} doors."
 
 
 if __name__ == "__main__":  # pragma: no cover
@@ -12,29 +13,16 @@ if __name__ == "__main__":  # pragma: no cover
 
 
 # -- HERE BE TESTS -- #
-import pytest
 
 
 def test_describe_car():
-    corsa = {"wheels": 4, "doors": 5}
+    corsa = Car(wheels=4, doors=5)
     assert describe_car(corsa) == "This car has 4 wheels and 5 doors."
-
-
-def test_bad_parameters():
-
-    with pytest.raises(Exception) as exc:
-        describe_car({})
-
-    assert str(exc.value) == "wheels is a required key"
-
-    with pytest.raises(Exception) as exc:
-        describe_car({"wheels": 4})
-
-    assert str(exc.value) == "doors is a required key"
 
 
 """ Scenario:
 $ pytest --cov=run --cov-report html --cov-branch run.py
 $ open htmlcov/index.html
+$ mypy run.py
 $ python run.py
 """
