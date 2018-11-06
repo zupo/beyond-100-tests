d36dd650bb5efd11d8f6df3d925f4e59ed67a2d0
Nejc Zupan <nejc.zupan@gmail.com>
2018-11-06 17:50:32 +0900

    flake8_assertive, step 3: there we go
    The test errors now tell us what is actually wrong.

--- run.py
+++ run.py
@@ -8,25 +8,26 @@ import unittest
 
 class TestCase(unittest.TestCase):
     def test_equal(self):
-        self.assertTrue(number() == 1)
+        self.assertEqual(number(), 1)
 
     def test_not_equal(self):
-        self.assertTrue(number() != 2)
+        self.assertNotEqual(number(), 2)
 
     def test_less_than(self):
-        self.assertTrue(number() < 2)
+        self.assertLess(number(), 2)
 
     def test_greater_than(self):
-        self.assertTrue(number() > 0)
+        self.assertGreater(number(), 0)
 
     def test_contained(self):
-        self.assertTrue(number() in [0, 1])
+        self.assertIn(number(), [0, 1])
 
     def test_instance(self):
-        self.assertTrue(isinstance(number(), int))
+        self.assertIsInstance(number(), int)
 
 
 """ Scenario:
 $ pytest run.py
 $ flake8 run.py
+$ open https://pypi.org/project/flake8-assertive/
 """
