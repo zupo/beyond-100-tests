f5b588806c274c371c33f67198b889fab74087c2
Nejc Zupan <nejc.zupan@gmail.com>
2018-11-06 15:55:46 +0900

    Mocking time, step 1: Mock, mock, mocks!

--- run.py
+++ run.py
@@ -1,23 +1,20 @@
-# fmt: off
-class ModelFeatureFactoryToolkitFeelsLikeJava(object):
-
-    with_a_very_long_parameter = "that is one long text, really"
-    and_another_one = "and a bit shorter text, maybe",
-
-    def some_very_complex_method_here(argument_foo, argument_bar):
-        return (
-            argument_foo ** argument_bar + with_a_very_long_parameter  # noqa
-        ) / and_another_one  # noqa
-# fmt: on
+from datetime import datetime
 
 
-def test_parameters_are_of_equal_length():
-    obj = ModelFeatureFactoryToolkitFeelsLikeJava
-    assert len(obj.with_a_very_long_parameter) == len(obj.and_another_one)
+def get_current_microsecond():
+    return datetime.now().microsecond
+
+
+# -- HERE BE TESTS -- #
+from unittest import mock
+
+
+@mock.patch("run.datetime")
+def test_get_current_microsecond(patched_dt):
+    patched_dt.now.return_value.microsecond = 999
+    assert get_current_microsecond() == 999
 
 
 """ Scenario:
 $ pytest run.py
-$ flake8 run.py
-$ open https://pypi.org/project/flake8_tuple/
 """
